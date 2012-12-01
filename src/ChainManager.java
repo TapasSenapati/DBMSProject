1b83515893f8b14d806721b2f0a8a8f2f5ca447f
baggasumit <baggasumit@gmail.com>
2012-12-01 01:21:42 -0500

    Update src/ChainManager.java

--- src/ChainManager.java
+++ src/ChainManager.java
@@ -112,47 +112,4 @@ public class ChainManager{
 	    e.printStackTrace();
 	  }
   }
-}public class ChainManager{
-  public static void main(String[] args){
-    int swValue;
-    
-    do{
-      // Display menu graphics
-      System.out.println("===========================================================");
-      System.out.println("|   AVAILABLE MENU FOR CHAIN MANAGER                      |");
-      System.out.println("===========================================================");
-      System.out.println("|   Choose from one of the Options below:                 |");
-      System.out.println("|        1. Add a store                                   |");
-      System.out.println("|        2. Remove a store                                |");
-      System.out.println("|        3. Add new Merhandise                            |");
-      System.out.println("|        4. Remove a Merchandise                          |");
-      System.out.println("|        5. Change Retail Price                           |");
-      System.out.println("|        6. Exit                                          |");
-      System.out.println("===========================================================");
-      swValue=BooksAThousand.getIntFromShell("Please Select an option: ");    
-      // Switch construct
-      switch (swValue) {
-        case 1:
-          System.out.println("Option 1 selected");
-          break;
-        case 2:
-          System.out.println("Option 2 selected");
-          break;
-        case 3:
-          System.out.println("Option 3");
-          break;
-        case 4:
-          System.out.println("Option 4");
-          break;
-        case 5:
-          System.out.println("Option 5");
-          break;
-        case 6:
-          System.out.println("Option 6");
-          break;
-        default:
-          System.out.println("Invalid selection");
-      }
-    }while(swValue!=6);
-  }
 }
